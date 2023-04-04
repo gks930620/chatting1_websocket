e776a56a686cfb9585abba3fc41e51a03314bd15
gks93 <gks930620@naver.com>
2023-04-04 15:40:17 +0900

    first

--- src/main/java/com/prac/prac/chat/WebSockChatHandler.java
+++ src/main/java/com/prac/prac/chat/WebSockChatHandler.java
@@ -27,6 +27,7 @@ public class WebSockChatHandler extends TextWebSocketHandler {
 
     }
 
+
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
@@ -42,7 +43,7 @@ public class WebSockChatHandler extends TextWebSocketHandler {
             chatMessage.setMessage(chatMessage.getSender() + "님이 퇴장했습니다..");
             sendToEachSocket(sessions,new TextMessage(objectMapper.writeValueAsString(chatMessage)) );
         }else {
-            sendToEachSocket(sessions,message ); //입장 아닐 때는 클라이언트로부터 온 메세지 그대로 전달.
+            sendToEachSocket(sessions,message ); //입장,퇴장 아닐 때는 클라이언트로부터 온 메세지 그대로 전달.
         }
     }
     private  void sendToEachSocket(Set<WebSocketSession> sessions, TextMessage message){
@@ -59,7 +60,7 @@ public class WebSockChatHandler extends TextWebSocketHandler {
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-       //javascript에서  session.close해서 연결 끊음. 그리고 이 메소드 실행.    그래서 여기서 session.close()해도 이미 없음.
+       //javascript에서  session.close해서 연결 끊음. 그리고 이 메소드 실행.
         //session은 연결 끊긴 session을 매개변수로 이거갖고 뭐 하세요.... 하고 제공해주는 것 뿐
         //내가 sessionID가지고  해당 session이 있던 room 찾아서  그 room의 sessions에서 session제거해줘도 되지만.  난 메세지부분으로 대체했다.
     }
